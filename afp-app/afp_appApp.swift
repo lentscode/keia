408aa83a0b59861c49a6db5d2f125fd46ca9b05e
lents <antonio.lentini2004@gmail.com>
2024-11-12 19:34:52 +0100

    Initial Commit

---
+++ afp-app/afp_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  afp_appApp.swift
+//  afp-app
+//
+//  Created by Antonio Lentini on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct afp_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ afp-appTests/afp_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  afp_appTests.swift
+//  afp-appTests
+//
+//  Created by Antonio Lentini on 12/11/24.
+//
+
+import Testing
+@testable import afp_app
+
+struct afp_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
