7fd4cd0039d0623aa027a931ed47e3b2daf70f6e
mmlst <listimiriam@gmail.com>
2024-11-23 10:46:17 +0100

    Added groupbox and groupboxStyle

--- keia/pages/history/HistoryView.swift
+++ keia/pages/history/HistoryView.swift
@@ -11,28 +11,58 @@ struct HistoryView: View {
     
     var objects: [PurchaseIntent]
     var body: some View {
-            List{
+        VStack {
+            ScrollView{
                 ForEach(objects, id: \.id){ object in
-                    
-                    VStack(alignment: .leading) {
-                        Spacer()
-                        HStack{
-                            Text(object.product)
-                                .bold()
+                    GroupBox(label: Text(object.product)){
+                        HStack {
+                            VStack(alignment: .leading){
+                                Text("\(object.price, specifier: "%.2f")€")
+                                Text("20/09/2024")
+                            }
                             Spacer()
-                            Text("\(object.score, specifier: "%.2f")/10")
+                            Text("\(object.score, specifier: "%.2f")")
                         }
-                        Spacer()
-                        Text("\(object.price, specifier: "%.2f")€")
-                        Spacer()
                     }
+                    .groupBoxStyle(.items)
+                    
+                    
                 }
             }
-        
+        }
     }
-
 }
 
 #Preview {
     HistoryView(objects: [PurchaseIntent(product: "Mac Book Pro M4 Pro", price: 4200, score: 7.7, purchased: false), PurchaseIntent(product: "iPhone 16", price: 890, score: 8.9, purchased: false)])
 }
+
+struct objectsGroupBoxStyle : GroupBoxStyle {
+    var backgroundColor: UIColor = UIColor .systemGroupedBackground
+    var width: CGFloat = 325
+    var labelColor: UIColor = UIColor.label
+    var opacity: Double = 1
+    func makeBody(configuration: Configuration) -> some View {
+        VStack(alignment: .leading){
+            HStack {
+                configuration.label
+                    .font(Font.bold(.body)())
+                    .foregroundColor(Color(labelColor))
+                    //.padding()
+            }
+            configuration.content
+        }
+        .frame(width: width)
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 8, style: .continuous)
+            .fill(Color(backgroundColor)))
+        .opacity(opacity)
+    }
+}
+
+extension GroupBoxStyle where Self == objectsGroupBoxStyle{
+    static var items:  objectsGroupBoxStyle{
+        .init()
+    }
+}
+
