e74afdeaca7c9aac77bae4a5d06aca5b685cb568
francescoromeo-dev <romeofrancesco301@gmail.com>
2024-11-25 16:33:43 +0100

    dad

---
+++ keia/pages/stats/StatisticsViewPage.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsViewPage.swift
+//  keia
+//
+//  Created by Francesco Romeo on 25/11/24.
+//
+
+import SwiftUI
+
+struct StatisticsViewPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticsViewPage()
+}
