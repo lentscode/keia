b6c2df8d6e489f5f6f9e2cf84b3df0ddb710d196
lents <111151429+lentscode@users.noreply.github.com>
2024-11-25 14:29:25 +0100

    :construction: Edit Blog related files

--- keia/logic/BlogService.swift
+++ keia/logic/BlogService.swift
@@ -6,15 +6,20 @@
 //
 
 import Foundation
+import Yams
 
 class BlogService {
-    private let articles = [
-        Article(title: "Prova 1", category: "A", file: "d3"),
-        Article(title: "Prova 2", category: "B", file: "d3"),
-        Article(title: "Prova 3", category: "B", file: "d3"),
-        Article(title: "Prova 4", category: "C", file: "d3"),
-        Article(title: "Prova 5", category: "C", file: "d3"),
+    private var articles = [
+        Article(title: "Prova 1", category: "A", file: "d3", text: "", author: ""),
+        Article(title: "Prova 2", category: "B", file: "d3", text: "", author: ""),
+        Article(title: "Prova 3", category: "B", file: "d3", text: "", author: ""),
+        Article(title: "Prova 4", category: "C", file: "d3", text: "", author: ""),
+        Article(title: "Prova 5", category: "C", file: "d3", text: "", author: ""),
     ]
+    
+    init() {
+        loadArticles()
+    }
     
     func getArticlesByCategory(where filter: String? = nil) -> [String: [Article]] {
         var dict: [String: [Article]] = [:]
@@ -28,7 +33,91 @@ class BlogService {
         for article in sortedArticles {
             dict[article.category, default: []].append(article)
         }
-
+        
         return dict
     }
+    
+    private func loadArticles() {
+        let fm = FileManager()
+        
+        if let path = Bundle.main.resourcePath {
+            do {
+                let articlePaths = try fm.contentsOfDirectory(atPath: path)
+                
+                var articles: [Article] = []
+                
+                for articlePath in articlePaths {
+                    let metadata = getMetadata(filePath: articlePath)
+                    
+                    let content = getContent(filePath: articlePath)
+                    
+                    if let metadata {
+                        articles.append(
+                            Article(
+                                title: metadata.title,
+                                category: metadata.category,
+                                file: metadata.image,
+                                text: content,
+                                author: metadata.author
+                            )
+                        )
+                    }
+                }
+                
+                if !articles.isEmpty {
+                    self.articles = articles
+                }
+            } catch {
+                debugPrint("Error while loading files")
+            }
+        }
+    }
+    
+    private func getMetadata(filePath: String) -> MarkdownMetadata? {
+        do {
+            let content = try String(contentsOfFile: filePath, encoding: .utf8)
+            
+            if let frontMatterRange = content.range(of: "---\n", options: [.regularExpression]) {
+                let frontMatter = content[frontMatterRange.upperBound...].components(separatedBy: "---").first ?? ""
+                if let yamlData = frontMatter.data(using: .utf8) {
+                    let decoder = YAMLDecoder()
+                    let metadata = try decoder.decode(MarkdownMetadata.self, from: yamlData)
+                    return metadata
+                }
+            }
+        } catch {
+            return nil
+        }
+        return nil
+    }
+    
+    private func getContent(filePath: String) -> String {
+        var content = ""
+        do {
+            let fileContent = try String(contentsOfFile: filePath, encoding: .utf8)
+            
+            guard let frontMatterStart = fileContent.range(of: "---\n") else {
+                content = fileContent
+                return content
+            }
+            
+            guard let frontMatterEnd = fileContent.range(of: "---", options: [], range: frontMatterStart.upperBound..<fileContent.endIndex) else {
+                content = fileContent
+                return content
+            }
+            
+            content = String(fileContent[frontMatterEnd.upperBound...]).trimmingCharacters(in: .whitespacesAndNewlines)
+        } catch {
+            print("Errore durante il caricamento del contenuto: \(error)")
+        }
+        
+        return content
+    }
+}
+
+fileprivate struct MarkdownMetadata: Decodable {
+    let title: String
+    let author: String
+    let category: String
+    let image: String
 }
--- keia/models/Article.swift
+++ keia/models/Article.swift
@@ -1,9 +1,11 @@
 import Foundation
 
-//Struct to define which values ​​BlogItems accepts
+// Struct to define which values ​​BlogItems accepts
 struct Article: Identifiable {
     var id = UUID()
     let title: String
     let category: String
     let file: String
+    let text: String
+    let author: String
 }
